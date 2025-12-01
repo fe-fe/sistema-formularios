0644fa311af1e3c38cdc3ca9e68a802e9e7752f0
thiago cordeiro <thiagocordeiro@bne.com.br>
2025-12-01 17:03:27 -0300

    feat: post processos avaliativos

--- src/main/java/com/sistemaformulario/views/CriarFormularioViewServlet.java
+++ src/main/java/com/sistemaformulario/views/CriarFormularioViewServlet.java
@@ -7,21 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/admin/formulario-novo")
+@WebServlet("/admin/forms")
 public class CriarFormularioViewServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-
-        String processoId = request.getParameter("processoId");
-
-        if (processoId == null || processoId.isEmpty()) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "É necessário selecionar um Processo Avaliativo primeiro.");
-            return;
-        }
-
         request.getRequestDispatcher("/WEB-INF/views/admin/cadastro_formulario.html").forward(request, response);
     }
 }
